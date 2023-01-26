d8e8b7baea8e9484bc541fbda36cb93658a34a30
Callum Macalast <calmacalast@gmail.com>
2023-01-26 18:14:44 +0000

    [IMP] Create user, skater model, set relationship

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,4 +42,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function skater()
+    {
+        return $this->hasOne(Skater::class);
+    }
 }
