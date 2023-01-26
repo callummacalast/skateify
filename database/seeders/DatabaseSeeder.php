c8e2fcc15401c9a732a46ded58a9c0281ab037a0
Callum Macalast <calmacalast@gmail.com>
2023-01-26 18:13:45 +0000

    [IMP] Create seeders for models

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(10)->create();
+
+        $this->call(RoleSeeder::class);
+
+        $this->call(AdminSeeder::class);
+        $this->call(UserSkaterSeeder::class);
+        $this->call(SkaterSeeder::class);
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
---
+++ database/seeders/SkaterSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Skater;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class SkaterSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Skater::create([
+            'user_id' => User::all()->random()->id,
+            'stance' => fake()->randomElement(['Goofy', 'Regular']),
+            'skill_level' => fake()->randomElement(['Beginner', 'Advanced', 'Professional']),
+            'deck_size' => fake()->numberBetween(7, 9),
+        ]);
+    }
+}
