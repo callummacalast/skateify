62b1a7890079036d79f3de15e1d3bcc95d562221
Callum Macalast <calmacalast@gmail.com>
2023-01-26 18:15:04 +0000

    [IMP] Create skater table

---
+++ database/migrations/2023_01_25_175300_create_skaters_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('skaters', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('stance');
+            $table->string('skill_level');
+            $table->string('deck_size');
+            $table->string('location')->nullable();
+            $table->dateTime('dob')->nullable();
+            $table->string('style')->nullable();
+            $table->string('type')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('skaters');
+    }
+};
