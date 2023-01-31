d0bfdd0e0fd9468d030511ae2ec93189d0d1fbdd
Callum Macalast <calmacalast@gmail.com>
2023-01-31 22:11:18 +0000

    [IMP] load skaters with relationship

--- app/Http/Controllers/SkatersController.php
+++ app/Http/Controllers/SkatersController.php
@@ -10,7 +10,7 @@ class SkatersController extends Controller
 {
     public function index(Skater $skater)
     {
-        $skater = User::with('skater')->find(1);
+        $skater = User::with('skater')->get();
         return view('skaters.index', compact('skater'));
     }
 }
