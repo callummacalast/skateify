d6512630ff6e57522c167237eef3df753eca7e7a
Callum Macalast <calmacalast@gmail.com>
2023-02-16 22:37:11 +0000

    [IMP] refactor layout partials

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,6 +13,8 @@
 
 </head>
 @include('layouts.header')
+<body>
+    
 <section
     class="relative bg-[url(https://images.unsplash.com/photo-1542727934-07691d6ebf0e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1568&q=80)] bg-cover bg-center bg-no-repeat">
     <div class="absolute inset-0 bg-white/75 sm:bg-transparent sm:bg-gradient-to-r sm:from-white/95 sm:to-white/25">
@@ -46,5 +48,7 @@
     </div>
 </section>
 
+@include('layouts.footer')
+</body>
 
 </html>
