02c432da35856d0e5b5b8847c1174a9e7fbf07ff
Callum Macalast <calmacalast@gmail.com>
2023-02-16 22:35:41 +0000

    [IMP] admin user controller, list view and edit

---
+++ app/Http/Controllers/Admin/AdminUserController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class AdminUserController extends Controller
+{
+    public function index()
+    {
+        $users = User::all();
+        return view('admin.users.index', compact('users'));
+    }
+
+    public function edit(User $user)
+    {
+        return view('admin.users.edit', compact('user'));
+    }
+}
