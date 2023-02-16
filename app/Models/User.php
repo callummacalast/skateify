40881d889817ab9ce3f1c0071ce0e09a0b18485b
Callum Macalast <calmacalast@gmail.com>
2023-02-16 22:36:25 +0000

    [IMP] skate spots relationship

--- app/Models/User.php
+++ app/Models/User.php
@@ -47,4 +47,9 @@ class User extends Authenticatable
     {
         return $this->hasOne(Skater::class);
     }
+
+    public function skate_spots()
+    {
+        return $this->hasMany(SkateSpot::class);
+    }
 }
