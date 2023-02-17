de7e72496fe998c3f55887d7bf694ac5d643eba5
Callum Macalast <calmacalast@gmail.com>
2023-02-17 13:40:44 +0000

    [IMP] skatespot controller update, HTML views

--- app/Http/Controllers/Admin/AdminSkateSpotController.php
+++ app/Http/Controllers/Admin/AdminSkateSpotController.php
@@ -3,9 +3,19 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\SkateSpot;
 use Illuminate\Http\Request;
 
 class AdminSkateSpotController extends Controller
 {
-    //
+    public function index()
+    {
+        $skate_spots = SkateSpot::all();
+        return view('admin.skate-spots.index', compact('skate_spots'));
+    }
+
+    public function create()
+    {
+        return view('admin.skate-spots.create');
+    }
 }
