fd18b514485fbb6f4ca5ace68b33f0792e48c3e1
Callum Macalast <calmacalast@gmail.com>
2023-02-21 21:34:52 +0000

    [IMP] skate spot comment setup

---
+++ database/migrations/2023_02_18_181719_create_skate_spot_comments_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('skate_spot_comments', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->unsignedBigInteger('skatespot_id');
+            $table->foreign('skatespot_id')->references('id')->on('skate_spots');
+            $table->string('body');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('skate_spot_comments');
+    }
+};
